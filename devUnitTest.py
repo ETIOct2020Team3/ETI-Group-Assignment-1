7f325b1cd694b4f73dadb333d9102f8cfd5bf57e
harith md noor <harithmdnoor@gmail.com>
2021-02-15 16:31:49 +0800

    Update devUnitTest.py

--- devUnitTest.py
+++ devUnitTest.py
@@ -1,38 +1,34 @@
-import unittest
 import pytest
 from unittest import mock 
 import main as main
 import combatMenuFunctions as combat
 import classes as classes
 
-class test_harith(unittest.TestCase):
-    def test_exit_pass(self):
-        passed = False
-        try:
-            main.exitGame()
-        except SystemExit as e: 
-            if e.code == 0:
-                passed = True
+def test_exit_pass():
+    passed = False
+    try:
+        main.exitGame()
+    except SystemExit as e: 
+        if e.code == 0:
+            passed = True
 
-        assert passed==True
+    assert passed==True
 
-    def test_move_pass(self):
-        original_input = mock.builtins.input
-        mock.builtins.input = lambda _: "D"
-        hero = classes.Player()
-        world = classes.World()
-        combat.UserMovementOption(hero,world)
-        assert world.world_map[hero.positionX][hero.positionY]==" " 
-        mock.builtins.input = original_input
+def test_move_pass():
+    original_input = mock.builtins.input
+    mock.builtins.input = lambda _: "D"
+    hero = classes.Player()
+    world = classes.World()
+    combat.UserMovementOption(hero,world)
+    assert world.world_map[hero.positionX][hero.positionY]==" " 
+    mock.builtins.input = original_input
 
 
-    def test_attack_pass(self):
-        rat= classes.Rat()
-        hero=classes.Player()
-        ratStartingHP = rat.hp
-        combat.attack(rat,hero)
-        assert ratStartingHP>rat.hp 
+def test_attack_pass():
+    rat= classes.Rat()
+    hero=classes.Player()
+    ratStartingHP = rat.hp
+    combat.attack(rat,hero)
+    assert ratStartingHP>rat.hp 
 
             
-if __name__ == '__main__':
-    unittest.main()
